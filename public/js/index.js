232edfcc80a04884a70caf117e86c6c9ff6fdd33
Joshua Navi <jnavi@ucsd.edu>
2016-04-25 20:30:14 -0700

    Specific county data now shows up to the right

--- public/js/index.js
+++ public/js/index.js
@@ -26,7 +26,7 @@ getCountyData = function(agency) {
       console.log(err);
       return;
     }
-    console.log(data);
+    makeDelphiChart2(data);
   });
 }
 
@@ -66,6 +66,67 @@ makeDelphiChart = function(data) {
     .attr("y", function(d) { return innerHeight - (innerHeight*(d / maxRating)); })
     .attr("height", function(d) { return innerHeight*d/maxRating;  })
     .on("click", function(d, i) { getCountyData(data[i].agency); })
+    .style("fill", function(d) { return getColor(d, maxRating); });
+  // Orient the x and y axis
+  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+  var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+  // TODO: Append X axis
+  chart
+    .append("g")
+    .attr("transform", "translate(" + 0 + "," + innerHeight + ")")
+    .call(xAxis)
+    .selectAll("text")
+    .attr("transform", "rotate(" + -45 + ")")
+    .style("text-anchor", "end");
+
+  // TODO: Append Y axis
+  chart
+    .append("g")
+    .call(yAxis);
+
+};
+
+makeDelphiChart2 = function(data) {
+  var margin = {top: 20, right: 10, bottom: 100, left: 70},
+      width = 500 - margin.right - margin.left,
+      height = 700 - margin.top - margin.bottom;
+
+  var innerWidth  = width  - margin.left - margin.right;
+  var innerHeight = height - margin.top  - margin.bottom;
+  var maxRating = d3.max( data.map(function(d){ return parseInt(d.total); }) );
+
+  var xScale = d3.scale.ordinal().rangeRoundBands([0, innerWidth], 0);
+  var yScale = d3.scale.linear().range([0, innerHeight]);
+
+  // Define the chart
+  var remove = d3
+    .select(".chart2")
+    .select("svg")
+    .remove()
+
+  var chart = d3
+    .select(".chart2")
+    .append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" +  margin.left + "," + margin.right + ")");
+
+  // Render the chart
+  xScale.domain( data.map(function (d){ return d.charge_description; }) );
+  yScale.domain([maxRating, 0]);
+
+  chart
+    .selectAll(".bar")
+    .data(data.map(function(d){ return d.total; }) )
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("id", function (d, i){ return data[i].charge_description; })
+    .attr("x", function(d, i) { return ((innerWidth / data.length)*i) + 30; })
+    .attr("width", (innerWidth / data.length) - 50)
+    .attr("y", function(d) { return innerHeight - (innerHeight*(d / maxRating)); })
+    .attr("height", function(d) { return innerHeight*d/maxRating;  })
     .style("fill", function(d) { return getColor(d, maxRating); });
   // Orient the x and y axis
   var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
