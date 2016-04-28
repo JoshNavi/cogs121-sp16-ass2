1e1b2b091b2ed46f9f1a1e835a73cc6bc1b30bc0
Paul Kim <p9kim@ucsd.edu>
2016-04-27 20:31:42 -0700

    map testing

---
+++ public/js/map.js
@@ -0,0 +1,58 @@
+(function(d3) {
+  "use strict";
+
+  var map = L.map('mapid', { zoomControl: false }).setView([32.969, -117.334], 10);
+
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    maxZoom: 18,
+    id: 'p9kim.ppgijk1l',
+    accessToken: 'pk.eyJ1IjoicDlraW0iLCJhIjoiY2luaGkweGh5MHZwMnU4a2p4aXAxaTh3ayJ9.v238swAmvFaC86OpYv7HXA'
+  }).addTo(map);
+
+  map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.keyboard.disable();
+
+var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+    g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+d3.json("./data/san-diego.geojson", function(error, collection) {
+  if (error) throw error;
+
+  var transform = d3.geo.transform({point: projectPoint}),
+      path = d3.geo.path().projection(transform);
+
+  var feature = g.selectAll("path")
+      .data(collection.features)
+    .enter().append("path");
+
+  map.on("viewreset", reset);
+  reset();
+
+  // Reposition the SVG to cover the features.
+  function reset() {
+    var bounds = path.bounds(collection),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+
+    svg .attr("width", bottomRight[0] - topLeft[0])
+        .attr("height", bottomRight[1] - topLeft[1])
+        .style("left", topLeft[0] + "px")
+        .style("top", topLeft[1] + "px");
+
+    g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+    feature.attr("d", path);
+  }
+
+  // Use Leaflet to implement a D3 geometric transformation.
+  function projectPoint(x, y) {
+    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+  }
+});
+
+})(d3);
