2f69078b4eb342158aa3f92b01b024b433b07910
Jeremy Leu <j3leu@ucsd.edu>
2016-04-28 20:04:08 -0700

    .

--- public/js/map.js
+++ public/js/map.js
@@ -170,7 +170,6 @@ d3.json("https://raw.githubusercontent.com/Saebyuckbaan/cogs121-sp16-ass2/master
     .on("click", function(d){ getCommunityCrimes(d.properties.NAME); } )
     .on("mouseover", function(d){ printInfo(d.properties.NAME, data); } );
 
-  var cities = svg.append("g").atrr("class", "cityDiv");
 
   map.on("viewreset", reset);
   reset();
